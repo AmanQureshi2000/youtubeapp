8e7c0d033feddb5183713ddf4d03ef005aa981d6
AmanQureshi <aman.qureshi@indiamart.com>
2025-07-26 21:15:08 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -100,6 +100,13 @@ app.get('/api/videos', async (req, res) => {
 });
 
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
